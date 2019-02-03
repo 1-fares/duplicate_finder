c5f17ad47f12b5de2cb693e95f7d3906a5971cde
Mike Mikerson <mike@mikerson.com>
2019-02-03 21:58:00 +0000

    rename to clarify python version; add 'else' to except

--- duplicate_finder.py
+++ duplicate_finder.py
@@ -3,9 +3,11 @@ import os
 import hashlib
 
 if len(sys.argv) < 2 or not os.path.isdir(sys.argv[1]):
-	print "Usage:"
-	print " duplicate_finder.py <path>"
+	print ("Usage:")
+	print (" duplicate_finder.py <path>")
 	sys.exit()
+
+print ("Scanning " + sys.argv[1] + " ...")
 
 hashes = {}
 nodupes = True
@@ -18,8 +20,8 @@ def read_in_chunks(file_object, chunk_size=1024*1024*16):
 		if not data:
 			break
 		if not first_iter:
-			sys.stdout.write('.')
-			sys.stdout.flush()
+			sys.stderr.write('.')
+			sys.stderr.flush()
 		first_iter = False
 		yield data
 
@@ -32,25 +34,27 @@ def get_file_hash(file_object):
 for (dirpath, dirnames, filenames) in os.walk(sys.argv[1]):
 	for filename in filenames:
 		filecount += 1
-		sys.stdout.write('O')
-		sys.stdout.flush()
+		sys.stderr.write('O')
+		sys.stderr.flush()
 		fullpath = ""
 		try:
 			fullpath = os.path.join(dirpath,filename)
 			filesize = os.path.getsize(fullpath)
 			if filesize == 0:
 				continue
-			file = open(fullpath, "r")
+			file = open(fullpath, "rb")
 			hash = get_file_hash(file)
 			file.close()
+#		except (IOError, PermissionError) as e:
 		except Exception as e:
 			error_filename = fullpath if len(fullpath) > 0 else filename
-			print
-			print str(filename) + " " + type(e).__name__ + ": " + str(e)
-		hashes.setdefault(str(hash) + str(filesize), list()).append(fullpath)
+			print ()
+			print (str(filename) + " " + type(e).__name__ + ": " + str(e))
+		else:
+			hashes.setdefault(str(hash) + "_" + str(filesize), list()).append(fullpath)
 
-if filecount > 0: print
-print str(filecount) + " file(s) examined"
+if filecount > 0: print ()
+print (str(filecount) + " file(s) examined")
 
 for key in hashes.keys():
 	dupes = hashes[key]
@@ -59,8 +63,8 @@ for key in hashes.keys():
 	nodupes = False
 	print(str(len(dupes)) + " duplicates with hash: " + key)
 	for filename in dupes:
-		print filename
-	print
+		print (filename)
+	print ()
 
 if filecount > 1 and nodupes:
-	print "No duplicates found"
+	print ("No duplicates found")
